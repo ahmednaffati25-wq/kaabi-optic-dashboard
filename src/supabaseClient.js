c5a52bffb8ef72774a8fe5066754a192aa6ccf28
ahmednaffeti <ahmed.naffati25@gmail.com>
2026-02-14 22:18:03 +0100

    prepare encoding fix

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+// UTF-8 Fixed
+import { createClient } from '@supabase/supabase-client'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
